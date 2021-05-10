50dc61fcc5f535ecb41f7754c575a8240810bbd3
naudecle <58804372+naudecle@users.noreply.github.com>
2021-05-10 22:15:32 +0400

    Add files via upload

---
+++ Projet_ECommerce/js/index.js
@@ -0,0 +1,22 @@
+/*===NAVIGATION===*/
+/*const navOpen = document.querySelector(".nav_hamburger");
+const navClose = document.querySelector(".close_toogle");
+const menu = document.querySelector(".nav_menu");
+const navContainer = document.querySelector(".nav_menu");
+
+navOpen.addEventListener("click", ()=>{
+    menu.classList.add("open");
+    document.body.classList.add("active");
+    navContainer.style.left = "0";
+    navContainer.style.width = "30rem";
+})
+navClose.addEventListener("click", ()=>{
+    menu.classList.add("open");
+    document.body.classList.remove("active");
+    navContainer.style.left = "-30rem";
+    navContainer.style.width = "0";
+})*/
+
+
+//Animation Scrolling
+AOS.init();
